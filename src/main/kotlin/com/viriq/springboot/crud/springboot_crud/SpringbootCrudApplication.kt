3f0b3e77908373eb4f9b9fd3e619d6ff498ee03f
viriq <vi.riquelmea@duocuc.cl>
2025-05-19 11:11:56 -0400

    Repositorio creado

---
+++ src/main/kotlin/com/viriq/springboot/crud/springboot_crud/SpringbootCrudApplication.kt
@@ -0,0 +1,11 @@
+package com.viriq.springboot.crud.springboot_crud
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootCrudApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringbootCrudApplication>(*args)
+}
