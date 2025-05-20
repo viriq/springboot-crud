290fddbaff39326d3c0f7ac0e80505cb37f4498f
viriq <vi.riquelmea@duocuc.cl>
2025-05-20 11:30:16 -0400

    Repositorio Vaciado 1

--- src/main/kotlin/com/viriq/springboot/crud/springboot_crud/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.viriq.springboot.crud.springboot_crud.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import com.viriq.springboot.crud.springboot_crud.entities.Product;
-
-public interface ProductRepository extends CrudRepository<Product, Long> {
-
-}
